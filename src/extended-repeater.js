42abf98d3b6dca4660f0df1ddbc1a58521f9f813
zarg336 <70839905+zarg336@users.noreply.github.com>
2020-11-01 22:36:26 +0400

    feat: solve extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,27 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
   
+module.exports = function repeater(str, options) {
+  let resultString = [];
+  let substring = [];
+  let separator;
+  let additionSeparator;
+  let repeatTimes;
+  let additionRepeatTimes;
+  
+  repeatTimes = typeof(options.repeatTimes) === 'number' ?  options.repeatTimes : 1;
+  separator = (options.separator) ?  options.separator : '+';
+  additionSeparator = (options.additionSeparator) ? options.additionSeparator : '+';
+  if (typeof(options.additionRepeatTimes) === 'number'){
+    additionRepeatTimes = options.additionRepeatTimes;
+  } else {
+    additionRepeatTimes = (options.addition) ? 1 : 0;
+  } 
+
+  for (let i = 0; i < additionRepeatTimes; i += 1) {
+    substring.push(`${options.addition}`);
+  }
+  for (let i = 0; i < repeatTimes; i += 1) {
+    resultString.push(str+substring.join(`${additionSeparator}`)) ;
+  }
+
+  return resultString.join(`${separator}`);
+};
